b06e53c26b460044e865d2af51d7e932fb70daa0
YOANGELICA <maria_a.portocarrero@uao.edu.co>
2023-11-17 15:06:10 -0500

    ignore api_endpoint.py

--- api_dag/api_endpoint.py
+++
@@ -1,1 +0,0 @@
-API_ENDPOINT = "https://api.powerbi.com/beta/693cbea0-4ef9-4254-8977-76e05cb5f556/datasets/dc4db27f-7306-4d18-adc2-c43c035ead74/rows?experience=power-bi&key=Tbrdvr5CbrhaZ8M7Jjm68iVm4vy5hM5DJwsuRl19XhKtQsuNVtnKAwVm8TTkYCEhstUDWLLgMUhAcALc13y3Ow%3D%3D"
